ebb26d00b47ed26c8481035f4624dfe3dc3ca659
NJHunter <130200115+NJHunter@users.noreply.github.com>
2023-07-28 10:29:09 -0400

    Update degree.h

---
+++ degree.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+
+//define the enumeration
+enum DegreeProgram 
+	{ 
+		SECURITY, NETWORK, SOFTWARE, NONE
+	};
+
+//this defines an array with the name degreeString
+static const std::string degreeString[] = { "SECURITY", "NETWORK", "SOFTWARE", "NONE"};
